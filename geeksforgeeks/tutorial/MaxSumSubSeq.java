4db6ec5b81349c842829ea122719056f3c1a9141
Varun <varun.v2021@gmail.com>
2018-05-02 17:15:16 +0530

    Update MaxSumSubSeq.java

--- geeksforgeeks/tutorial/MaxSumSubSeq.java
+++ geeksforgeeks/tutorial/MaxSumSubSeq.java
@@ -25,39 +25,22 @@ public class MaxSumSubSeq {
 		}
 		// sort the stack in natural order to get highest at top
 		Collections.sort(stack);
-		if(!stack.isEmpty())
 		System.out.println("Max sum of sub sequence is: " + stack.pop());
 	}
 
-	public static void main1(String[] args) {
-		// Note that size of arr[] is considered 100 according to
-		// the constraints mentioned in problem statement.
-		int[] arr = new int[100];
-
-		// Input the number of test cases you want to run
-		Scanner sc = new Scanner(System.in);
-		if(sc.hasNextInt()) {
-			int t = sc.nextInt();
-
-			// One by one run for all input test cases
-			while (t > 0) {
-				// Input the size of the array
-				int n = sc.nextInt();
-
-				// Input the array
-				for (int i = 0; i < n; i++)
-					arr[i] = sc.nextInt();
-
-				// Input the element to be searched
-				int x = sc.nextInt();
-
-				MaxSumSubSeq subSeq = new MaxSumSubSeq();
-				// Compute and print result
-				subSeq.getMaxSum(arr, arr.length);
-
-				t--;
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		for (int i = 0; i < t; i++) {
+			int n = s.nextInt();
+			int[] ar = new int[n];
+			for (int j = 0; j < n; j++) {
+				ar[j] = s.nextInt();
 			}
+
+			MaxSumSubSeq subSeq = new MaxSumSubSeq();
+			// Compute and print result
+			subSeq.getMaxSum(ar, ar.length);
 		}
-		sc.close();
 	}
 }
