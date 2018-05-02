72c2c90c2c9faff5342b82358d204b732462cc6f
Varun <varun.v2021@gmail.com>
2018-05-02 17:51:15 +0530

    Add files via upload

---
+++ geeksforgeeks/tutorial/ZigZag.java
@@ -0,0 +1,31 @@
+package com.sample.geeksforgeeks.tutorial;
+
+import java.util.Arrays;
+
+public class ZigZag {
+	public static void zigZagArrangement(int[] arr) {
+		boolean flag = true;
+		for (int i = 0; i <= arr.length - 2; i++) {
+			if (flag) {
+				if (arr[i] > arr[i + 1]) {
+					int temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+				}
+			} else {
+				if (arr[i] < arr[i + 1]) {
+					int temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+				}
+			}
+			flag = !flag;
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = new int[] { 4, 3, 7, 8, 6, 2, 1 };
+		ZigZag.zigZagArrangement(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+}
